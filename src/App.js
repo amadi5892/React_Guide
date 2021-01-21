abfdfc97933e4e85a8c8f341de7ae32ca7410c0c
Parrish Williamson <williamsonparrish@gmail.com>
2021-01-20 19:13:07 -0500

    useState lesson

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ],
+    otherState: 'some other value'
+  });
+
+  const switchNameHandler = () => {
+        // console.log('Was clicked!')
+        // DON'T DO THIS! this.state.persons[0].name = 'Maximilian';
+        setPersonsState({
+          persons: [
+        {name: 'Maximilian', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 27}]
+        })
+      }
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+    </div>
+  )
 }
 
-export default App;
+export default app;
+
+// You can have multiple useState calls
+
+// import React, { Component } from 'react';
+// import './App.css';
+// import Person from './Person/Person';
+
+// class App extends Component {
+//   state = {
+//     persons: [
+//       {name: 'Max', age: 28},
+//       {name: 'Manu', age: 29},
+//       {name: 'Stephanie', age: 26}
+//     ]
+//   }
+
+//   switchNameHandler = () => {
+//     // console.log('Was clicked!')
+//     // DON'T DO THIS! this.state.persons[0].name = 'Maximilian';
+//     this.setState({persons: [
+//     {name: 'Maximilian', age: 28},
+//     {name: 'Manu', age: 29},
+//     {name: 'Stephanie', age: 27}]
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hi, I'm a React App</h1>
+//         <p>This is really working!</p>
+//         <button onClick={this.switchNameHandler}>Switch Name</button>
+//         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+//         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+//         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+//       </div>
+//     );
+//     // return React.createElement('div',{className: 'App'}, React.createElement('h1',null, 'Hi I\'m a React App!!!'))
+//   }
+// }
+
+// export default App;
+
+// state is only usable in extended components

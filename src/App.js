e8b2dd96da094c909fecc88297e4f1369225294f
Parrish Williamson <williamsonparrish@gmail.com>
2021-01-20 20:13:38 -0500

    different way to conditionally render elements

--- src/App.js
+++ src/App.js
@@ -87,16 +87,11 @@ class App extends Component {
       cursor: 'pointer'
     }
   
+    let persons = null;
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button 
-        style={style}
-        onClick={this.togglePersonsHandler}>Switch Name</button>
-        {this.state.showPersons ?
-          <div>
+    if (this.state.showPersons) {
+      persons = (
+        <div>
             <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age}/>
@@ -108,8 +103,19 @@ class App extends Component {
             <Person 
             name={this.state.persons[2].name} 
             age={this.state.persons[2].age}/>
-          </div> : null
-          }
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button 
+        style={style}
+        onClick={this.togglePersonsHandler}>Switch Name</button>
+        {persons} 
+        
         
       </div>
     );

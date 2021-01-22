039ce0bb197ca579977fb113fdbc5b89ccf9f942
Parrish Williamson <williamsonparrish@gmail.com>
2021-01-22 18:08:06 -0500

    lifecycle cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -73,7 +73,8 @@ class App extends Component {
       {id: 'kidid', name: 'Stephanie', age: 26}
     ],
     otherState: 'some other value',
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -148,11 +149,12 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit 
+        <button onClick={() => {this.setState({showCockpit: false})}}>Remove Cockpit</button>
+        {this.state.showCockpit ? (<Cockpit 
         showPersons={this.state.showPersons} 
         persons={this.state.persons}
         clicked={this.togglePersonsHandler}
-        title={this.props.appTitle} />
+        title={this.props.appTitle} />) : null}
         {persons}
       </div>
     );

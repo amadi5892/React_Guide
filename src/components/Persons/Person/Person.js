1a3d931f88e1ebcc3597dd51f462cf1366e7cdf6
Parrish Williamson <williamsonparrish@gmail.com>
2021-01-22 12:25:43 -0500

    useEffect

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
 
 
-const person = (props) => {
-    console.log('[Person.js] rendering...')
+class Person extends Component {
+    render() {
+        console.log('[Person.js] rendering...')
+
+        return (
+            // <div className="Person" style={style}>
+            <div className={classes.Person}>
+                <p onClick={this.props.click} >I'm a {this.props.name} and I am {this.props.age} years old!!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        )
+    }
+    
     // const style = {
     //     '@media (min-width: 500px)': {
     //         width: '450px'
     //     }
     // }
 
-    return (
-        // <div className="Person" style={style}>
-        <div className={classes.Person}>
-            <p onClick={props.click} >I'm a {props.name} and I am {props.age} years old!!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    )
+    
 }
 
-export default person;
+export default Person;
 
 // a component is a function returning some JSX

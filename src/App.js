d68cc1afc46915481d79d831e1c348b705cf10f1
Parrish Williamson <williamsonparrish@gmail.com>
2021-01-21 21:32:39 -0500

    css modules

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@
 // You can have multiple useState calls
 
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 // import styled from 'styled-components';
 
@@ -102,20 +102,9 @@ class App extends Component {
 
   render() {
     // inline styling
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    }
   
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -142,26 +131,31 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // }
+
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    
+
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      assignedClasses.push(classes.red); // classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red', 'bold']
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button 
+        className={btnClass}
         alt={this.state.showPersons}
         onClick={this.togglePersonsHandler}>Switch Name</button>
         {persons} 

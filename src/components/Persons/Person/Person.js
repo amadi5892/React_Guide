700b7b567e356aa095c73d3bb3cce7c191039d3f
Parrish Williamson <williamsonparrish@gmail.com>
2021-01-27 11:44:28 -0500

    ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,16 @@ import withClass from '../../../hoc/withClass';
 
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...')
 
@@ -15,7 +25,9 @@ class Person extends Component {
              {/* <div className="Person" style={style}></div> */}
                 <p onClick={this.props.click} >I'm a {this.props.name} and I am {this.props.age} years old!!</p>,
                 <p>{this.props.children}</p>,
-                <input type="text" 
+                <input type="text"
+                // ref={(inputEl) => {this.inputElement = inputEl}}
+                ref={this.inputElementRef} 
                 onChange={this.props.changed} 
                 value={this.props.name} />
             </Auxillary>
